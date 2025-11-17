03a925593ea1c0017d2c15a7df454fabf5b7a032
¨danielcosta010¨ <105571748+danielcosta010@users.noreply.github.com>
2025-11-17 08:32:58 -0300

    função chamada imediatamente

--- Conhcendo Fuções JS/01-O poder das funções/imc-com-funcoes.js
+++ Conhcendo Fuções JS/01-O poder das funções/imc-com-funcoes.js
@@ -1,24 +1,31 @@
 function calcularIMC(peso, altura) {
-    return peso / Math.pow(altura, 2);
+  return peso / Math.pow(altura, 2);
 }
 
 function classificaImc(imc) {
-    if (imc < 18.5) {
-    console.log(`Seu IMC (índice de massa corporal) é ${imc}, você está abaixo do peso.`)
-} else if(imc >= 18.5 && imc < 25) {
-    console.log(`Seu IMC (índice de massa corporal) é ${imc}, você está dentro do peso.`)
-} else if (imc >= 25 && imc < 30) {
-    console.log(`Seu IMC (índice de massa corporal) é ${imc}, você está acima do peso.`)   
-} else if (imc >= 30 && imc < 40) {
-    console.log(`Seu IMC (índice de massa corporal) é ${imc}, você está obeso.`)   
-} else {
-    console.log(`Seu IMC (índice de massa corporal) é ${imc}, você com obesidade mórbida.`)   
+  if (imc < 18.5) {
+    return `Seu IMC (índice de massa corporal) é ${imc}, você está abaixo do peso.`;
+  } else if (imc >= 18.5 && imc < 25) {
+    return `Seu IMC (índice de massa corporal) é ${imc}, você está dentro do peso.`;
+  } else if (imc >= 25 && imc < 30) {
+    return `Seu IMC (índice de massa corporal) é ${imc}, você está acima do peso.`;
+  } else if (imc >= 30 && imc < 40) {
+    return `Seu IMC (índice de massa corporal) é ${imc}, você está obeso.`;
+  } else {
+    return `Seu IMC (índice de massa corporal) é ${imc}, você com obesidade mórbida.`;
+  }
 }
-}
 
-const peso = 70;
-const altura = 1.65;
-const imc = calcularIMC(100, 1.65);
-console.log(imc);
+// function main() {
+//   const peso = 70;
+//   const altura = 1.65;
+//   const imc = calcularIMC(100, 1.65);
+//   console.log(classificaImc(imc));
+// }
 
-
+(function () {
+  const peso = 70;
+  const altura = 1.65;
+  const imc = calcularIMC(100, 1.65);
+  console.log(classificaImc(imc));
+})();
