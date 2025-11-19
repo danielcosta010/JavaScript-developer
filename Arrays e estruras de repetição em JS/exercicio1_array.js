d512f52ea698bb2479f7ea21b42d97c3b96ff210
¨danielcosta010¨ <105571748+danielcosta010@users.noreply.github.com>
2025-11-19 09:39:42 -0300

    exercicio1 de array

---
+++ Arrays e estruras de repetição em JS/exercicio1_array.js
@@ -0,0 +1,8 @@
+// Crie um programa que imprima uma tabuada.
+
+let numero = 7;
+for (let i = 0; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} X ${i} = ${resultado}`); 
+}
+
